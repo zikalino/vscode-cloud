78cd7f0a0cf6f0464f364be914e7320917ad76d3
Zim Kalinowski <zim.kalinowski@espressif.com>
2024-08-17 12:44:58 +0800

    parsing full help

--- src/extension.ts
+++ src/extension.ts
@@ -201,65 +201,100 @@ async function parseCmdHelp() {
 
   // here we can process all the lines and add all the necessary stuff
 
-  // XXX - skip the command and go to arguments
   i = 0;
-  while (i < lines.length) {
-    if (lines[i] === "# Arguments") {
-      i++;
-      break;
-    }
-    i++;
-  }
-
-  lines.splice(i - 1, 0, "type: layout-form",
-                         "header: ",
-                         "  - type: header",
-                         "    title: Virtual Machine",
-                         "    logo: icon.webp",
-                         "form:",
-                         "  - type: fieldset",
-                         "    subitems:");
-  i += 8;
-
-  // XXX - search for first argument
-  while (!lines[i].startsWith("#     --")) {
-    i++;
-  }
-
-  // XXX - go through all arguments
   while (true) {
-    var j = i;
-    // XXX - get argument name & other things
-    var name = lines[j].split("--")[1].split(" ")[0];
-    j++;
-    while (lines[j].startsWith("#       ")) {
-      j++;
-    }
-
-    while (i < j) {
-      // insert indented comment
-      lines[i] = "      " + lines[i];
-      i++;
-    }
-    // insert argument information
-    lines.splice(j, 0, "      - type: row",
-                       "        subitems: ",
-                       "          - type: textfield",
-                       "            name: " + name,
-                       "            produces: ",
-                       "              - variable: " + name.replaceAll("-", "_"));
-      i += 6;
-    
-    if (!lines[i].startsWith("#     --")) {
+    i = parseCmdHelp_FindNextSection(lines, i);
+    if (i < 0) {
       break;
     }
 
+    if (lines[i].endsWith("Command")) {
+      // XXX - for now do nothing, but extract command description and name
+      i++;
+      continue;
+    } else if (lines[i].endsWith("Global Arguments")) {
+      // remove global arguments
+      j = parseCmdHelp_FindNextSection(lines, i + 1);
+      lines.splice(i, j - i);
+      continue;
+    } else if (lines[i].endsWith("Examples")) {
+      // examples should stay as they are now
+      i++;
+      continue;
+    } else if (lines[i].endsWith("Arguments")) {
+      while (i < lines.length) {
+        if (lines[i] === "# Arguments") {
+          i++;
+          break;
+        }
+        i++;
+      }
+
+      lines.splice(i - 1, 0, "type: layout-form",
+                            "header: ",
+                            "  - type: header",
+                            "    title: Virtual Machine",
+                            "    logo: icon.webp",
+                            "form:",
+                            "  - type: fieldset",
+                            "    subitems:");
+      i += 8;
+
+      // XXX - search for first argument
+      while (!lines[i].startsWith("#     --")) {
+        i++;
+      }
+
+      // XXX - go through all arguments
+      while (true) {
+        var j = i;
+        // XXX - get argument name & other things
+        var name = lines[j].split("--")[1].split(" ")[0];
+        j++;
+        while (lines[j].startsWith("#       ")) {
+          j++;
+        }
+
+        while (i < j) {
+          // insert indented comment
+          lines[i] = "      " + lines[i];
+          i++;
+        }
+        // insert argument information
+        lines.splice(j, 0, "      - type: row",
+                          "        subitems: ",
+                          "          - type: textfield",
+                          "            name: " + name,
+                          "            produces: ",
+                          "              - variable: " + name.replaceAll("-", "_"));
+          i += 6;
+        
+        if (!lines[i].startsWith("#     --")) {
+          break;
+        }
+
+      }
+    } else {
+      // unknown section just skip
+      i++;
+      continue;
+    }
   }
 
   r = lines.join("\r\n");
   vscode.window.activeTextEditor?.edit((editBuilder) => {
     editBuilder.insert(new vscode.Position(0, 0), r);
   });
+}
+
+function parseCmdHelp_FindNextSection(lines: string[], idx: number) {
+  while (idx < lines.length) {
+    if (lines[idx].length > 3 && lines[idx].startsWith("# ") && !lines[idx].startsWith("#  ")) {
+      return idx;
+    }
+    idx++;
+  }
+  return -1;
 }
 
 
